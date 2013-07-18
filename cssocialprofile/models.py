8e7b94ace32275c506f7d77fe479c0c75b0369a4
urtzai <uodriozola@codesyntax.com>
2013-07-18 16:10:03 +0200

    Pipelines created for a new social_auth integration

--- cssocialprofile/models.py
+++ cssocialprofile/models.py
@@ -6,10 +6,10 @@ from django.conf import settings
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext as _
 
-from social_auth.signals import pre_update
-from social_auth.backends.facebook import FacebookBackend
-from social_auth.backends.twitter import TwitterBackend
-from social_auth.backends import OpenIDBackend
+# from social_auth.signals import pre_update
+# from social_auth.backends.facebook import FacebookBackend
+# from social_auth.backends.twitter import TwitterBackend
+# from social_auth.backends import OpenIDBackend
 
 USERTYPE_CHOICES = getattr(settings,'USERTYPE_CHOICES', ((0,'Erabiltzailea'),(1,'Kidea'),(2,'Nor publikoa'),(3,'Kazetaria'),(4,'Administratzailea')))
 AUTH_PROFILE_MODULE = getattr(settings,'AUTH_PROFILE_MODULE', 'cssocialprofile.CSSocialProfile')
@@ -87,91 +87,4 @@ def create_profile(sender, instance, created,**kwargs):
         profile,new = model._default_manager.get_or_create(user=instance) 
 from django.db.models.signals import post_save
 post_save.connect(create_profile, sender=User)
-
-
-def get_facebook_photo(response):
-    """ """
-    import facebook.djangofb as facebook
-    
-    facebook = facebook.Facebook(settings.FACEBOOK_API_KEY, settings.FACEBOOK_API_SECRET)
-    uid = response.get('id')
-    user_data = facebook.users.getInfo(uid, ['first_name', 'last_name','pic_big',])[0]
-    if user_data:
-        img_url = user_data['pic_big']
-        img_title = u'Facebook: ' + user_data['first_name'] + u' ' + user_data['last_name']
-        return loadUrlImage(img_url,img_title)
-    else:
-        return None
-
-
-def get_twitter_photo(response):
-    """ """
-    img_url = response.get('profile_image_url')
-    img_url=img_url.replace('_normal.','.')
-    username = response.get('screen_name')
-    return loadUrlImage(img_url,u'twitter: ' + username)
-
-
-
-def facebook_extra_values(sender, user, response, details, **kwargs):
-    """ """
-    profile = user.get_profile()
-    profile.facebook_id = response.get('id')
-
-    if profile.usertype == 0:
-        profile.usertype = 1
-    
-    if profile.added_source == 0:
-        #First time logging in
-        profile.added_source = 3
-        profile.mota = 1
-    if not profile.photo:
-        profile.photo = get_facebook_photo(response)
-    profile.save()
-    #username in facebook users...
-    user.username = slugify(user.username)
-    user.save()
-    
-    return True
-
-pre_update.connect(facebook_extra_values, sender=FacebookBackend)
-
-
-def twitter_extra_values(sender, user, response, details, **kwargs):
-    """ """
-    model = get_profile_model()
-    profile,new = model._default_manager.get_or_create(user=user) 
-
-    if not profile.photo:
-        profile.photo = get_twitter_photo(response)
-    profile.twitter_id = response.get('screen_name','')
-
-    if profile.usertype == 0:
-        profile.usertype = 1
-    if profile.added_source == 0:
-        profile.added_source = 2
-
-    if not profile.bio:
-        profile.bio = response.get('description','')         
-
-    if not profile.fullname:
-        profile.fullname = response.get('name','')    
-
-    profile.save()
-    return True
-pre_update.connect(twitter_extra_values, sender=TwitterBackend)
-
-def openid_extra_values(sender, user, response, details, **kwargs):
-    """ """
-    profile = user.get_profile()
-
-    if response.status == 'success':
-        profile.openid_id = response.getDisplayIdentifier()
-        if profile.added_source == 0:
-            profile.mota = 1
-            profile.added_source = 4
-    profile.save()
-    return True
-
-pre_update.connect(openid_extra_values, sender=OpenIDBackend)
 
