ba22c2bdd4ea8f2d2f5aa1b259357abd46b549c5
csmant <csmant@anboto>
2018-04-10 11:52:15 +0200

    urls

--- cssocialprofile/urls.py
+++ cssocialprofile/urls.py
@@ -1,29 +1,28 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+from cssocialprofile import views
 
 
 #default view for our index
-urlpatterns = patterns('cssocialprofile.views',
-    url(r'^$','index', name="cssocialprofile_index"),
-    )
+urlpatterns = [
+    url(r'^$', views.index, name="cssocialprofile_index"),
+]
 
 #register and social urls
-urlpatterns += patterns('',
-    url(r'^logout$','django.contrib.auth.views.logout', name='cssocialprofile_logout'),
-    url(r'^login$','django.contrib.auth.views.login', name='cssocialprofile_user_login'),
+urlpatterns += [
+    url(r'^logout$', auth_views.logout, name='cssocialprofile_logout'),
+    url(r'^login$', auth_views.login, name='cssocialprofile_user_login'),
 
-    #url(r'^accounts/password/$',TemplateView.as_view(template_name='/')),
-    #url(r'^accounts/$',TemplateView.as_view(template_name='/')),
 
-    (r'^accounts/', include('registration.backends.default.urls')),
-    url('', include('social.apps.django_app.urls', namespace='social'))
-)
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url('', include('social_django.urls', namespace='social'))
+]
 
 #default profile edit urls
-urlpatterns += patterns('cssocialprofile.views',
-    url(r'^edit-profile$','edit_profile', name='cssocialprofile_edit_profile'),
-    url(r'^edit-profile-photo$','edit_profile_photo', name='cssocialprofile_edit_profile_photo'),    
-    url(r'^edit-profile-social$','edit_profile_social', name='cssocialprofile_edit_profile_social'),    
-)
-
+urlpatterns += [
+    url(r'^edit-profile$', views.edit_profile, name='cssocialprofile_edit_profile'),
+    url(r'^edit-profile-photo$', views.edit_profile_photo, name='cssocialprofile_edit_profile_photo'),
+    url(r'^edit-profile-social$', views.edit_profile_social, name='cssocialprofile_edit_profile_social'),
+]
 
