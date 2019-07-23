1647d33780cb728c5266b82f7440d61fbbe3da86
Urtzi Odriozola <uodriozola@codesyntax.com>
2019-07-23 16:12:19 +0200

    django 2.0 modifications

--- cssocialprofile/views.py
+++ cssocialprofile/views.py
@@ -13,26 +13,25 @@ from django.utils.translation import ugettext as _
 
 from .models import get_profile_model
 
+
 def index(request):
-    """ """
     h = {}
     return render_to_response('cssocialprofile/base.html', h, context_instance=RequestContext(request))
-    
-        
-        
+
+
+
 @login_required
 def edit_profile(request):
-    """ """
     tab = 'personal'
-    user= request.user
+    user = request.user
     profile_model = get_profile_model()
-    profile = getattr(user,profile_model.__name__.lower())
+    profile = getattr(user, profile_model.__name__.lower())
     if request.method == 'POST':
-        posta=request.POST.copy()     
+        posta = request.POST.copy()
         profileform = ProfileForm(posta, instance=profile)
         if profileform.is_valid():
             profileform.save()
-            messages.add_message(request, messages.SUCCESS, _('New user data saved.'), fail_silently=True)    
+            messages.add_message(request, messages.SUCCESS, _('New user data saved.'), fail_silently=True)
             return HttpResponseRedirect(reverse('cssocialprofile_edit_profile'))
     else:
         profileform = ProfileForm(instance=profile)
@@ -41,9 +40,8 @@ def edit_profile(request):
 
 
 def handle_uploaded_file(f, title):
-    """ """
     photo = Photo()
-    photo.title = u'%s %s' % (title, time_slug_string()) 
+    photo.title = u'%s %s' % (title, time_slug_string())
     photo.slug = time_slug_string()
     photo.image = f
     photo.save()
@@ -52,30 +50,26 @@ def handle_uploaded_file(f, title):
 
 @login_required
 def edit_profile_photo(request):
-    """ """
     tab = 'photo'
     user = request.user
     profile_model = get_profile_model()
-    profile = getattr(user,profile_model.__name__.lower())
+    profile = getattr(user, profile_model.__name__.lower())
     if request.method == 'POST':
         form = ProfilePhotoForm(request.POST, request.FILES)
         if form.is_valid():
             photo = handle_uploaded_file(request.FILES['avatarpic'], profile.get_fullname())
             profile.photo = photo
             profile.save()
-                    
+
     else:
-        form = ProfilePhotoForm()       
+        form = ProfilePhotoForm()
     return render_to_response('profile/edit_photo.html', locals(), context_instance=RequestContext(request))
-        
+
 
 @login_required
 def edit_profile_social(request):
-    """ """
     tab = 'social'
     user = request.user
     profile_model = get_profile_model()
-    profile = getattr(user,profile_model.__name__.lower())
+    profile = getattr(user, profile_model.__name__.lower())
     return render_to_response('profile/edit_social.html', locals(), context_instance=RequestContext(request))
-        
-
